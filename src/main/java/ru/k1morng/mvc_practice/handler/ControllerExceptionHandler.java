0801f1d068c3c6700971738f7dab2f941fe60f74
standaloneee <tzherzdev@gmail.com>
2022-08-09 13:06:00 +0300

    added ExceptionHandlers

--- src/main/java/ru/k1morng/mvc_practice/handler/ControllerExceptionHandler.java
+++ src/main/java/ru/k1morng/mvc_practice/handler/ControllerExceptionHandler.java
@@ -2,14 +2,35 @@ package ru.k1morng.mvc_practice.handler;
 
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.k1morng.mvc_practice.exception.UserNotFoundException;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+import java.util.HashMap;
+import java.util.Map;
+
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@RestControllerAdvice
 public class ControllerExceptionHandler extends Exception {
-    public ControllerExceptionHandler(String message) {
-        super(message);
+    //    public ControllerExceptionHandler(String message) {
+//        super(message);
+//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return errorMap;
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleUserNotFoundException(UserNotFoundException ex){
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        return errorMap;
     }
 }
