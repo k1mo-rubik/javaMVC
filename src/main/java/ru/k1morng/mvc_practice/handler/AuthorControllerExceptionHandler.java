379a8363b606d42c470f73b5edf476969e71b91b
standaloneee <tzherzdev@gmail.com>
2022-08-09 21:17:44 +0300

    NoAuthorFoundException не реализован

--- src/main/java/ru/k1morng/mvc_practice/handler/AuthorControllerExceptionHandler.java
+++ src/main/java/ru/k1morng/mvc_practice/handler/AuthorControllerExceptionHandler.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.k1morng.mvc_practice.exception.EmptyPageException;
-import ru.k1morng.mvc_practice.exception.UserNotFoundException;
+import ru.k1morng.mvc_practice.exception.AuthorIsDeletedException;
+import ru.k1morng.mvc_practice.exception.InvalidBookToAuthorException;
+import ru.k1morng.mvc_practice.exception.NoAuthorFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +19,7 @@ import java.util.Map;
 public class AuthorControllerExceptionHandler extends Exception {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+    protected Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
         Map<String, String> errorMap = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
             errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
@@ -25,24 +27,41 @@ public class AuthorControllerExceptionHandler extends Exception {
         return errorMap;
     }
 
-    @ExceptionHandler(UserNotFoundException.class)
+    @ExceptionHandler(AuthorIsDeletedException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleUserNotFoundException(UserNotFoundException ex){
+    protected Map<String, String> handleUserNotFoundException(AuthorIsDeletedException ex) {
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("errorMessage", ex.getMessage());
         return errorMap;
     }
 
-//    @ExceptionHandler(EmptyPageException.class)
+    //    @ExceptionHandler(EmptyPageException.class)
 //    protected ResponseEntity<EmptyPageException> handleEmptyPageException(){
 //        return new ResponseEntity<>(new EmptyPageException("This page is empty"), HttpStatus.FORBIDDEN);
 //    }
     @ExceptionHandler(EmptyPageException.class)
     @ResponseStatus(HttpStatus.SEE_OTHER)
-    protected Map<String, String> handleEmptyPageException(EmptyPageException ex){
+    protected Map<String, String> handleEmptyPageException(EmptyPageException ex) {
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("Error", ex.getMessage());
         return errorMap;
     }
-//
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    protected Map<String, String> handleInvalidBookToAuthorException(InvalidBookToAuthorException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("Error", ex.getMessage());
+        return errorMap;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.SEE_OTHER)
+    protected Map<String, String> handleNoAuthorFoundException(NoAuthorFoundException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("Error", ex.getMessage());
+        return errorMap;
+    }
+
+
 }
