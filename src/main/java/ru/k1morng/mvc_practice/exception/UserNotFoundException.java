de38039787fcc4704bf3206afdac54d8cc1086d7
standaloneee <tzherzdev@gmail.com>
2022-08-09 21:19:43 +0300

    not used yet

--- src/main/java/ru/k1morng/mvc_practice/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.k1morng.mvc_practice.exception;
-
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
